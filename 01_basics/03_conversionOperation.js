1e16f786b5b0160ba1f38b6c6a1d97c45b2556f2
Dk74041mehta <dilipmehta70876@gmail.com>
2024-08-10 20:53:45 +0530

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,44 +29,67 @@ let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
-
-
 // *********************** Operations ***********************
 
-let value = 786
+let value = 3
 let negValue = -value
-// console.log(negValue);
+console.log(negValue);
 
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
-// console.log(2**3);
+// console.log(2**3);   // 2 raise to power 2
 // console.log(2/3);
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " hitesh"   //for space
 
-let str3 = str1 + str2
+let str3 = str1 + str2  //adding strings
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);    //12
+// console.log(1 + "2");     //12
+// console.log("1" + 2 + 2);   //122
+// console.log(1 + 2 + "2");    //32
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true);  //1
+// console.log(+"");  //0
 
 let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
 
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
+//1) postfix Increment (++) operator
+
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// // x2 is 4n; y2 is 3n
+
+//2) Prefix increment
+
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+
+
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
