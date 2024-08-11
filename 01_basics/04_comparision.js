d9b09ff6ecec7e6b3b0136443306c2b19b865e94
Dk74041mehta <dilipmehta70876@gmail.com>
2024-08-11 13:54:08 +0530

    comparison

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -4,18 +4,21 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
+ 
+// console.log("2" > 1);   //true becuase because engine convert string into number automaticaly
+// console.log("02" > 1); 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// *** Avoid These Type of Conversions ***
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(null > 0); //false
+console.log(null == 0); //flase
+console.log(null >= 0); //true
 
-// === 
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0);        //false
 
-console.log("2" === 2);
+// ===   strict check
+
+console.log("2" === 2);  //also check data-types 
