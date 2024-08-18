ff1898af0a6206d4e2e4b466d20d6759b3b5ac5b
Dk74041mehta <dilipmehta70876@gmail.com>
2024-08-18 21:30:58 +0530

    string.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,35 +1,49 @@
-const name = "hitesh"
-const repoCount = 50
+// string can be declare AS using  ' ' or  " "
 
-// console.log(name + repoCount + " Value");
+// const name = "Dilip Mehta "
+// const repoCount = 50
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(name+repoCount + "  Value");  //old techniques
 
-const gameName = new String('hitesh-hc-com')
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  //today techniques string interpulation
+
+const gameName = new String('Dilip Mehta') //new syntax to declare string [using constructor() and object]
+
+// console.log(gameName);
+
+
 
 // console.log(gameName[0]);
-// console.log(gameName.__proto__);
+// console.log(gameName.__proto__);   //accessing prototype -op-object{}
 
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+// console.log(gameName.charAt(1));
+// console.log(gameName.indexOf('i'));  
 
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+// const newString = gameName.substring(0, 11)  //start and ending number which is not execluded
+// console.log(newString);
 
-const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const anotherString = gameName.slice(-10, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   Dilip   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim()); //remove starting and empty space
+
+
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace('%20', '-'))  //changing value and updating value
 
-console.log(url.includes('sundar'))
+console.log(url.includes('sundar'))  //checking value present in array or not
 
-console.log(gameName.split('-'));
+console.log(gameName.split(' ')); //spreter and limiterf
+
+
+
+
+
